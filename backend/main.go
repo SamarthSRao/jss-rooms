da38a2a59004ece1d9f0dbb9267f07e4e4c98606
Samarrrtthh <Samarthz0901@gmail.com>
2026-01-17 23:28:51 +0530

    Revert "cors"
    This reverts commit a4d70345ced360ca2b9e0d0a4b6285a2b485ab99.

--- backend/main.go
+++ backend/main.go
@@ -119,22 +119,13 @@ func main() {
 	mux.HandleFunc("/api/events", handleEvents) // We'll handle role check inside here for GET/POST mix
 	mux.HandleFunc("/ws", handleWebSocket)
 
-	// Improved CORS wrapper
+	// Simple CORS wrapper
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		} else {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Private-Network", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
